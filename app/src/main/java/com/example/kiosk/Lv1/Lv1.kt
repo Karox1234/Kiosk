6aab151928c241909cb2fe2b128a58b0e8a053df
Karox1234 <zzzcomet@gmail.com>
2023-12-12 16:08:22 +0900

    세부 메뉴판 수정

--- app/src/main/java/com/example/kiosk/Lv1/Lv1.kt
+++ app/src/main/java/com/example/kiosk/Lv1/Lv1.kt
@@ -5,12 +5,18 @@ fun main() {
 
     while (true) {
         println("원하시는 메뉴를 선택하세요")
-        println("1.스테이크류 , 2.구이류 , 3.음료 및 주류 , 4.종료")
+        println("1.스테이크류 | 육즙이 넘치는 스테이크!")
+        println("2.구이류 | 맛있는 고기들을 한 자리에!")
+        println("3.음료 및 주류 | 찰떡궁합 음료와 주류들!")
+        println("4.종료 | 프로그램을 종료합니다")
         val menuPanel = readLine()
         when (menuPanel) {
             "1" -> {
                 println("고기 부위를 선택하세요")
-                println("1.안심 , 2.등심 , 3.T본 , 4.전 메뉴판으로 돌아가기")
+                println("1.안심 | W 11.9 | 스테이크의 정석 안심부위")
+                println("2.등심 | W 10.9 | 마블링이 풍부한 등심")
+                println("3.T본 | W 12.9 | 풍부한 맛을 즐기고 싶으신분")
+                println("4.돌아가기 | 전 메뉴판으로 돌아갑니다")
                 val stakeSelect = readLine()
                 when (stakeSelect) {
                     "1" -> {
@@ -39,7 +45,10 @@ fun main() {
             }
             "2" -> {
                 println("구위 부위를 선택하세요")
-                println("1.삼겹살 , 2.목살 , 3.갈매기살 , 4.전 메뉴판으로 돌아가기")
+                println("1.삼겹살 | W 6.9 | 한국인의 소울푸드")
+                println("2.목살 | W 5.9 | 기름이 적어 담백한 그맛")
+                println("3.갈매기살 | W 7.9 | 오늘의 특수부위")
+                println("4.돌아가기 | 전 메뉴판으로 돌아갑니다")
                 val meatSelect = readLine()
                 when (meatSelect) {
                     "1" -> {
@@ -68,7 +77,11 @@ fun main() {
 
             "3" -> {
                 println("음료 및 주류를 선택하세요")
-                println("1.탄산음료 , 2.맥주 , 3.소주 , 4.와인 , 5.전 메뉴판으로 돌아가기")
+                println("1.탄산음료 | W 2.5 | 청량감 최고")
+                println("2.맥주 | W 2.5 | 목으로 넘어가는 시원한 한잔")
+                println("3.소주 | W 4.0 | 삼겹살엔 소주 한잔")
+                println("4.와인 | W 8.9 | 스테이크와 함께하는 최고의 선택")
+                println("5.돌아가기 | 전 메뉴판으로 돌아갑니다")
                 val drinkSelect = readLine()
                 when (drinkSelect) {
                     "1" -> {
